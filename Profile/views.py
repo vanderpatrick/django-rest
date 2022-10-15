cf9a29f8ad1d12b79d44e938be4b477a7c9f8e3a
vanderpatrick <patrick123321123@icloud.com>
2022-10-15 09:34:37 +0000

    add more stuf

--- Profile/views.py
+++ Profile/views.py
@@ -4,33 +4,37 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Profile
 from .serializers import profileSerializer
+from drf_api.permission import IsOwnerOrReadOnly
 
 
 class ProfileList(APIView):
     def get(self, request):
         profiles = Profile.objects.all()
-        serializer = profileSerializer(profiles, many=True)
+        serializer = profileSerializer(profiles, many=True,
+        context={'request': request})
 
         return Response(serializer.data)
 
 
 class ProfileDetail(APIView):
     serializer_class = profileSerializer
+    permission_classes = [IsOwnerOrReadOnly]
     def get_object(self, pk):
         try:
             profile = Profile.objects.get(pk=pk)
+            self.check_object_permissions(self.request, profile)
             return profile
         except Profile.DoesNotExist:
             raise Http404
 
     def get(self, request, pk):
         profile = self.get_object(pk)
-        serializer = profileSerializer(profile)
+        serializer = profileSerializer(profile, context={'request': request})
         return Response(serializer.data)
 
     def put(self, request, pk):
         profile = self.get_object(pk)
-        serializer = profileSerializer(profile, data=request.data)
+        serializer = profileSerializer(profile, data=request.data, context={'request': request})
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -17,5 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth', include('rest_framework.urls')),
     path('', include('Profile.urls')),
 ]
