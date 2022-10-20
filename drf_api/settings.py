c5672c01250dd2fb2adcbb7c70697c84380bc82d
vanderpatrick <patrick123321123@icloud.com>
2022-10-20 14:14:04 +0000

    a

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -103,7 +103,7 @@ INSTALLED_APPS = [
 ]
 SITE_ID = 1
 MIDDLEWARE = [
-    'corsheader.middleware.CorsMiddlleware',
+    'corsheaders.middleware.CorsMiddlleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
