7ff33bca9ec7308bb1d69bdf888853e0ba5c727a
vanderpatrick <patrick123321123@icloud.com>
2022-11-03 18:33:58 +0000

    adad

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = "DEV" in os.environ
 
 ALLOWED_HOSTS = [
-     os.environ.get('ALLOWED_HOST'),
+     'skt-drf.herokuapp.com',
      'localhost'
 ]
 
